594a5c7ca67c31b8a296d37ca828197384a9ac9b
RicardoWorf <steve4137@gmail.com>
2020-04-16 22:30:51 -0300

    Frontend Application

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { } from 'react-icons/fa';
+
+import './global.css';
+
+function App() {
+
+	return (
+		<div className="app">
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+export default App;
