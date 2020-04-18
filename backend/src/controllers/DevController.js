5af997a2fb060bc653bdb78bc48f02d00b65f7a4
RicardoWorf <steve4137@gmail.com>
2020-04-17 22:37:06 -0300

    Fix errors of not exist

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,16 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
 module.exports = {
+    async index(req, res) {
+        const response = await Dev.find();
+        
+        console.log(response);
+        return res.json(response);
+    },
+
     async store(req, res) {
         const { github_username, techs } = req.body;
 
@@ -9,17 +18,76 @@ module.exports = {
 
         const { name, bio, avatar_url } = response.data;
 
-        const arrayAsString = techs.split(', ').map(tech => tech.trim());
+        const techsArray = parseStringAsArray(techs);
 
         dev = await Dev.create({
             name,
             github_username,
             bio,
             avatar_url,
-            techs: arrayAsString
+            techs: techsArray
         });
 
         return res.json(dev);
 
+    },
+
+    async show(req, res) {
+        const { github_username } = req.params;
+
+        const githubExist = await Dev.findOne({ github_username });
+
+        if(githubExist) {
+            const response = await axios(`https://api.github.com/users/${github_username}/repos`);    
+
+            return res.json(response.data);
+        }
+
+        return res.status(404).json({ "Error": "Error not found Github user subscribe, try another" });
+
+        
+
+        
+    },
+
+    async update(req, res) {
+        const { _id } = req.params;
+
+        const idExist = await Dev.findOne({_id});
+
+        if(idExist) {
+            const { name, avatar_url, bio, techs } = req.body;
+
+            const techsArray = parseStringAsArray(techs);
+
+            await Dev.updateOne({ _id }, {
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+            });
+
+            return res.status(204).send();
+        }
+
+        return res.status(401).json({ "error": "Not exist this Github user, try another." });
+
+        
+    },
+
+    async destroy(req, res) {
+        const { _id } = req.params;
+
+        const idExist = await Dev.findOne(_id);
+
+        if(idExist) {
+            await Dev.deleteOne(idExist); 
+            
+            return res.status(204).send();
+        }
+
+        return res.status(401).json({ "error": "Not exist this Github user, try another." });
+
+       
     }
 };
