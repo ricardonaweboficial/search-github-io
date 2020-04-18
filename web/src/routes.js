fa6e9830ac178d4be717429c6a3b16b6f891ccd4
RicardoWorf <steve4137@gmail.com>
2020-04-17 22:43:08 -0300

    Create Routes with paths

---
+++ web/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import About from './pages/About';
+import Repos from './pages/Repos';
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={About} />
+                <Route path="/repos" component={Repos} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
