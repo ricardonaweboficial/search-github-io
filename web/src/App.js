3e103285c1573f790de0b08e6e7238a448d060a2
RicardoWorf <steve4137@gmail.com>
2020-04-17 22:38:57 -0300

    Modification for router

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { } from 'react-icons/fa';
-
 import './global.css';
 
-function App() {
+import Routes from './routes';
 
+function App() {
 	return (
-		<div className="app">
-			<h1>Hello World</h1>
-		</div>
+		<Routes />
 	);
 }
 
