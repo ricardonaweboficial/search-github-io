ff3b492344ca4a059570cc5738d597bc1309c0bc
RicardoWorf <steve4137@gmail.com>
2020-04-17 22:37:43 -0300

    Fix routes params

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,10 @@ const DevControllers = require('./controllers/DevController');
 
 const routes = Router();
 
+routes.get('/', DevControllers.index);
+routes.post('/users/:github_username', DevControllers.show);
 routes.post('/users', DevControllers.store);
+routes.put('/users/:_id', DevControllers.update);
+routes.delete('/users/:id', DevControllers.destroy);
 
 module.exports = routes;
