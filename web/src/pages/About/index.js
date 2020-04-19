3978fca890eaa265ccb9b44f2a60028d78639edb
RicardoWorf <steve4137@gmail.com>
2020-04-18 22:40:06 -0300

    Add Integration API for create devs and list devs

--- web/src/pages/About/index.js
+++ web/src/pages/About/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaChevronRight  } from 'react-icons/fa';
 
 import { Link } from 'react-router-dom';
 
 import github_img from '../../assets/github_img.png';
+
+import api from '../../services/api';
 
 import {
     DivContainer,
@@ -19,16 +21,57 @@ import {
 } from './styles';
 
 export default function About() {
+    const [ devs, setDevs ] = useState([]);
+
+    const [ github_username, setGithub_username ] = useState('');
+    const [ techs, setTechs ] = useState('');
+
+    useEffect(() => {
+        async function loadDevs() {
+            try {
+                const response = await api.get('/');
+
+                setDevs(response.data);
+                console.log(response.data);
+            } catch (err) {
+                return alert('Error in load devs');
+            }
+        }
+        loadDevs();
+    }, []);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/users', {
+                github_username,
+                techs,
+            });
+
+            setDevs(...devs, response.data);
+        } catch (err) {
+            return alert('Error in create dev, try another.');
+        }
+    }
+
     return (
         <DivContainer>
             <Header>
                 <h1>SEARCH GITHUB IO</h1>
             </Header>
                 <Section>
-                    <FormDev>
+                    <FormDev onSubmit={handleSubmit}>
                         <h1>CADASTRAR DEV</h1>
-                        <input placeholder="Usuário do Github"/>
-                        <input placeholder="Tecnologias"/>
+                        <input
+                            
+                            value={github_username}
+                            onChange={e => setGithub_username(e.target.value)} 
+                            placeholder="Usuário do Github"/>
+                        <input
+                            value={techs}
+                            onChange={e => setTechs(e.target.value)} 
+                            placeholder="Tecnologias"/>
                         <button type="submit">CADASTRAR</button>
                     </FormDev>
                         
@@ -38,7 +81,7 @@ export default function About() {
                             <img src={github_img} alt="Github Avatar"/>
                             <DivInfo>
                                 <h3>Ricardo Santos</h3>
-                                <p>Desenvolvedor Web |  Criando projetos com ReactJS e NodeJS e sempre estudando para consumir ainda mais conhecimentos.</p>
+                                <p>Desenvolvedor Web | Criando projetos com ReactJS e NodeJS e sempre estudando para consumir ainda mais conhecimentos.</p>
                                 <p>Tecnologias: <br /><Techs>ReactJS, Node.js</Techs></p>
                                 <DivGroupInfo>
                                     <p>Github: <Github>RicardoWorf</Github></p>
