1517408645c26c22cd5f4832c69fb4694c27f89c
RicardoWorf <steve4137@gmail.com>
2020-04-20 19:47:31 -0300

    Feature for circle color language

--- web/src/pages/Repos/index.js
+++ web/src/pages/Repos/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
+import { FaStar, FaCircle, FaFileAlt, FaChevronLeft } from 'react-icons/fa';
 
-import { FaStar, FaCircle, FaFileAlt, FaChevronLeft } from 'react-icons/fa'
+import LanguageColor from '../../utils/LanguageColor';
+
+import api from '../../services/api';
 
 import {
     DivContainer,
@@ -15,41 +18,56 @@ import {
     Footer
 } from './styles';
 
-export default function Repos() {
+export default function Repos() {   
+    const { github_username } = useParams();
+
+    const [ repos, setRepos ] = useState([]);
+
+    const [ colorCircle, setColorCircle ] = useState('');
+
+    useEffect(() => {
+        async function loadRepos() {
+            try {
+                const response = await api.post(`users/${github_username}`); 
+
+                setRepos(response.data);
+                console.log(response.data);
+            } catch (err) {
+                return alert('Erro ao buscar repositorio, tente novamente');
+            }
+
+            
+        }
+
+        loadRepos();
+    }, []);
+
     return (
         <DivContainer>
             <Header>
                 <Link to="/"><ButtonBack><FaChevronLeft size={16} color="#2f2f2f"/>  Back</ButtonBack></Link>
-                <h1>Repositório do <span>Ricardo Santos</span></h1>
+                <h1>Repositório do <span>{github_username}</span></h1>
             </Header>
             <Section>
                 <ListRepos>
-                    <ItemRepo>
-                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
-                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
-                        <DivGroupInfo>
-                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
-                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
-                        </DivGroupInfo>
-                    </ItemRepo>
+                    {repos.map(repo => (
+                        <ItemRepo key={repo.id}>
+                            <h1><FaFileAlt size={16} color="#6f6f6f"/><a href={`https://www.github.com/${github_username}/${repo.name}`}>{repo.name}</a></h1>
+                            <p>{repo.description === null ? repo.description = 'Sem descrição' : repo.description}</p>
+                            <DivGroupInfo>
+                                {repo.language ? (
+                                    <Fragment>
+                                        <h4><FaCircle color={LanguageColor(repo.language)} size={16}/>{repo.language === null ? repo.language = 'Sem Linguagem' : repo.language}</h4>
+                                        <h4><FaStar size={16} color="#ED464B"/> {repo.stargazers_count}</h4>   
+                                    </Fragment>
+                                ) : (
+                                    <h1></h1>
+                                )}
+                                
+                            </DivGroupInfo>
+                        </ItemRepo>                  
 
-                    <ItemRepo>
-                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
-                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
-                        <DivGroupInfo>
-                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
-                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
-                        </DivGroupInfo>
-                    </ItemRepo>
-
-                    <ItemRepo>
-                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
-                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
-                        <DivGroupInfo>
-                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
-                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
-                        </DivGroupInfo>
-                    </ItemRepo>                    
+                    ))}
                 </ListRepos>
             </Section>
             <Footer>
