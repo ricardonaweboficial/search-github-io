7a60e3656908ae2be93067963ac24b420b564d52
RicardoWorf <steve4137@gmail.com>
2020-04-19 22:17:22 -0300

    Finish jsx for page of repos

--- web/src/pages/Repos/index.js
+++ web/src/pages/Repos/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import { FaStar, FaCircle, FaFileAlt, FaChevronLeft } from 'react-icons/fa'
+
+import {
+    DivContainer,
+    Header,
+    ButtonBack,
+    Section,
+    ListRepos,
+    ItemRepo,
+    DivGroupInfo,
+    Footer
+} from './styles';
+
 export default function Repos() {
     return (
-        <h1>Repos</h1>
+        <DivContainer>
+            <Header>
+                <Link to="/"><ButtonBack><FaChevronLeft size={16} color="#2f2f2f"/>  Back</ButtonBack></Link>
+                <h1>Repositório do <span>Ricardo Santos</span></h1>
+            </Header>
+            <Section>
+                <ListRepos>
+                    <ItemRepo>
+                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
+                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
+                        <DivGroupInfo>
+                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
+                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
+                        </DivGroupInfo>
+                    </ItemRepo>
+
+                    <ItemRepo>
+                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
+                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
+                        <DivGroupInfo>
+                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
+                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
+                        </DivGroupInfo>
+                    </ItemRepo>
+
+                    <ItemRepo>
+                        <h1><FaFileAlt size={16} color="#6f6f6f"/> Dashboard</h1>
+                        <p>Dashboard with Login and Tasks, using ReactJS, NodeJS, SQLite -> Query Builder</p>
+                        <DivGroupInfo>
+                            <h4><FaCircle color="yellow" size={16}/>Javascript</h4>
+                            <h4><FaStar size={16} color="#ED464B"/> 2</h4>
+                        </DivGroupInfo>
+                    </ItemRepo>                    
+                </ListRepos>
+            </Section>
+            <Footer>
+                <h1>CREATED BY: <span>RicardoWorf</span></h1>
+            </Footer>
+        </DivContainer>
     );
 }
