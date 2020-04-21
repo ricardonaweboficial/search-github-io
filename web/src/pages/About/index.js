d939e5e924678791baf4de7ce5d4a03bf7726f72
RicardoWorf <steve4137@gmail.com>
2020-04-21 20:27:37 -0300

    Fix in components of css

--- web/src/pages/About/index.js
+++ web/src/pages/About/index.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaChevronRight, FaTrash  } from 'react-icons/fa';
 
-import { Link } from 'react-router-dom';
-
-import github_img from '../../assets/github_img.png';
+import { useHistory } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -13,7 +11,9 @@ import {
     Section,
     FormDev,
     ListDevs,
+    DistributeP,
     DivInfo,
+    ButtonDelete,
     Techs,
     DivGroupInfo,
     Footer
@@ -24,6 +24,8 @@ export default function About() {
 
     const [ github_username, setGithub_username ] = useState('');
     const [ techs, setTechs ] = useState('');
+
+    const history = useHistory();
 
     useEffect(() => {
         async function loadDevs() {
@@ -66,6 +68,10 @@ export default function About() {
         } catch (err) {
             return alert('Erro ao excluir Dev, tente novamente.');
         }
+    } 
+
+    function handleRepos(dev) {
+        return history.push(`/repos/${dev}`);
     }
 
     return (
@@ -92,15 +98,15 @@ export default function About() {
                         <h1>Desenvolvedores</h1>
                         {devs.map(dev => (
                             <li key={dev._id}>
-                                <button onClick={() => handleDeleteDev(dev._id)}><FaTrash size={16} color="#ED464B"/></button>
+                                <ButtonDelete onClick={() => handleDeleteDev(dev._id)}><FaTrash size={16} color="#ED464B"/></ButtonDelete>
                                 <img src={dev.avatar_url} alt={dev.name}/>
                                 <DivInfo>
                                     <h3>{dev.name === null ? dev.name = dev.github_username : dev.name}</h3>
-                                    <p>{dev.bio}</p>
+                                    <DistributeP>{dev.bio}</DistributeP>
                                     <p>Tecnologias: <br /><Techs>{dev.techs.join(', ')}</Techs></p>
                                     <DivGroupInfo>
                                         <p>Github: <a href={`https://www.github.com/${dev.github_username}`}>{dev.github_username}</a></p>
-                                        <Link to={`/repos/${dev.github_username}`}>REPOSITÓRIOS <FaChevronRight size={16} color="#ED464B"/></Link>
+                                        <button onClick={() => handleRepos(dev.github_username)}>REPOSITÓRIOS <FaChevronRight size={16} color="#ED464B"/></button>
                                     </DivGroupInfo> 
                                 </DivInfo>
                             </li>
@@ -114,3 +120,5 @@ export default function About() {
         </DivContainer>
     );
 }
+
+// Link to={`/repos/${dev.github_username}`}
