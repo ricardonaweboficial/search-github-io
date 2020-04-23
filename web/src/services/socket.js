93cfb0bab90cd7c98744e957638251b911160d22
RicardoWorf <steve4137@gmail.com>
2020-04-22 21:51:35 -0300

    Connection in database usign socket.io

---
+++ web/src/services/socket.js
@@ -0,0 +1,20 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://localhost:3333', {
+	autoConnect: false,
+});
+
+function connect() {
+	socket.connect();
+}
+
+function disconnect() {
+	if(socket.connected) {
+		socket.disconnect();
+	}
+}
+
+export {
+	connect,
+	disconnect,
+};
