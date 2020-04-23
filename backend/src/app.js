27c798c33d151f0a5fc26a83ecd461ba0b870ee9
RicardoWorf <steve4137@gmail.com>
2020-04-22 21:47:15 -0300

    import dependencie socket

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const cors = require('cors');
 const express = require('express');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://github_io:github147@cluster0-udzur.mongodb.net/github_io?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,5 +19,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-module.exports = app;
+server.listen(3333);
 
